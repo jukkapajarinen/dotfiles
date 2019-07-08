0250fabc612702127bbad3dd887c5f62bd4c26ce
Jukka Pajarinen <jukkapajarinen91@gmail.com>
2019-07-08 18:35:36 +0300

    Added alacritty and initial configuration file to dotfiles.

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -26,21 +26,26 @@ ln -s $scriptDir/git/.gitconfig $homeDir/.gitconfig;
 [ $macos == true ] && mkdir -p $homeDir/Library/Application\ Support/Code/User/;
 [ $macos == true ] && ln -s $scriptDir/vscode/settings.json $homeDir/Library/Application\ Support/Code/User/settings.json;
 [ $macos == true ] && ln -s $scriptDir/vscode/keybindings.json $homeDir/Library/Application\ Support/Code/User/keybindings.json;
+#TODO: add alacritty config symlink for macos
 
 # Create Linux-only symlinks
 [ $macos == false ] && mkdir -p $homeDir/.config/i3;
 [ $macos == false ] && mkdir -p $homeDir/.config/rofi;
+[ $macos == false ] && mkdir -p $homeDir/.config/alacritty;
 [ $macos == false ] && mkdir -p $homeDir/.config/Code/User/;
 [ $macos == false ] && ln -s $scriptDir/i3/config $homeDir/.config/i3/config;
 [ $macos == false ] && ln -s $scriptDir/rofi/config $homeDir/.config/rofi/config;
+[ $macos == false ] && ln -s $scriptDir/alacritty/alacritty.yml $homeDir/.config/alacritty/alacritty.yml;
 [ $macos == false ] && ln -s $scriptDir/xorg/Xresources $homeDir/.Xresources;
 [ $macos == false ] && ln -s $scriptDir/xorg/xsessionrc $homeDir/.xsessionrc;
 [ $macos == false ] && ln -s $scriptDir/xorg/Xmodmap $homeDir/.Xmodmap;
 [ $macos == false ] && ln -s $scriptDir/vscode/settings.json $homeDir/.config/Code/User/settings.json;
 [ $macos == false ] && ln -s $scriptDir/vscode/keybindings.json $homeDir/.config/Code/User/keybindings.json;
-[ $macos == false ] && ln -s $scriptDir/xdm/Xresources /etc/X11/xdm/Xresources;
 [ $macos == false ] && ln -s $scriptDir/apt/sources.list /etc/apt/sources.list;
 [ $macos == false ] && ln -s $scriptDir/apt/preferences /etc/apt/preferences;
+[ $macos == false ] && ln -s /usr/bin/firefox-dev /etc/alternatives/x-www-browser;
+[ $macos == false ] && ln -s /usr/bin/i3 /etc/alternatives/x-window-manager;
+[ $macos == false ] && ln -s /usr/bin/alacritty /etc/alternatives/x-terminal-emulator;
 
 # Print info that execution finished
 echo "Script execution ended."
