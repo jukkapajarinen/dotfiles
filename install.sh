2e1432fe91fb332c7df5b35cc7c4f11ba08c73bc
Jukka Pajarinen <jukkapajarinen91@gmail.com>
2019-11-03 22:01:30 +0200

    Flattened dotfiles file-hierarchy and refactored bash_profile and install.sh.

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Check if we run MacOS
-[ "$(uname -s)" == "Darwin" ] && macos=true || macos=false;
+[[ "$(uname -s)" == "Darwin" ]] && macos="true" || linux="true";
 
 # Declare other variables
 scriptDir=$(cd "$(dirname $0)" && pwd);
 homeDir=${scriptDir///dotfiles/};
 mkdir="mkdir -p";
 ln="ln -sv";
-cliOnly=false;
+cliOnly="false";
 
 # Print the directory paths
-echo "Host operating system is: $([ $macos == true ] && echo MacOS || echo Linux)";
+echo "Host operating system is: $([[ "$macos" == "true" ]] && echo MacOS || echo Linux)";
 echo "Script executed in: $scriptDir";
 echo "Home directory detected as: $homeDir";
 
@@ -24,7 +24,7 @@ fi
 # Read possible --cli-only from the cli args
 if [[ $1 == "--cli-only" ]] || [[ $2 == "--cli-only" ]]; then
   echo "Note: Only cli-configurations will be installed.";
-  cliOnly=true;
+  cliOnly="true";
 fi
 
 # Read possible --force or -f from the cli args
@@ -33,47 +33,51 @@ if [[ $1 == "--force" ]] || [[ $2 == "--force" ]] || [[ $1 == "-f" ]] || [[ $2 =
   [[ $yn =~ [yY](es)* ]] && ln="ln -svf" || exit 0;
 fi
 
-# Create directories
-[[ $cliOnly == false ]] && $mkdir $homeDir/.config/alacritty;
-[[ $cliOnly == false ]] && [[ $macos == true ]] && $mkdir $homeDir/Library/Application\ Support/Code/User/;
-[[ $cliOnly == false ]] && [[ $macos == true ]] && $mkdir $homeDir/Library/Application\ Support/Spectacle/;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $mkdir $homeDir/.config/i3;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $mkdir $homeDir/.config/rofi;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $mkdir $homeDir/.config/dunst;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $mkdir $homeDir/.config/gtk-3.0;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $mkdir $homeDir/.config/Code/User/;
+# MacOS create directories
+[[ "$cliOnly" == "false" ]] && [[ "$macos" == "true" ]] && $mkdir $homeDir/Library/Application\ Support/Code/User/;
+[[ "$cliOnly" == "false" ]] && [[ "$macos" == "true" ]] && $mkdir $homeDir/Library/Application\ Support/Spectacle/;
 
-# Create symlinks
-$ln $scriptDir/bash/bash_profile $homeDir/.bash_profile;
-$ln $scriptDir/git/gitconfig $homeDir/.gitconfig;
-$ln $scriptDir/nano/nanorc $homeDir/.nanorc;
-[[ $macos == false ]] && $ln /usr/share/nano /usr/local/share/nano;
-[[ $cliOnly == false ]] && $ln $scriptDir/alacritty/alacritty.yml $homeDir/.config/alacritty/alacritty.yml;
-[[ $cliOnly == false ]] && [[ $macos == true ]] && $ln $scriptDir/vscode/settings.json $homeDir/Library/Application\ Support/Code/User/settings.json;
-[[ $cliOnly == false ]] && [[ $macos == true ]] && $ln $scriptDir/vscode/keybindings.json $homeDir/Library/Application\ Support/Code/User/keybindings.json;
-[[ $cliOnly == false ]] && [[ $macos == true ]] && $ln $scriptDir/spectacle.app/Shortcuts.json $homeDir/Library/Application\ Support/Spectacle/Shortcuts.json;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/i3/config $homeDir/.config/i3/config;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/conky/conkyrc $homeDir/.conkyrc;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/rofi/config $homeDir/.config/rofi/config;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/dunst/dunstrc $homeDir/.config/dunst/dunstrc;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/gtk/gtk2 $homeDir/.gtkrc-2.0;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/gtk/gtk3 $homeDir/.config/gtk-3.0/settings.ini;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/mime/mimeapps.list $homeDir/.config/mimeapps.list;
-[[ $macos == false ]] && $ln $scriptDir/keyboard/apple_keyboard_fi /etc/default/keyboard;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/keyboard/apple_keyboard_fi.xkbmap $homeDir/.xkbmap;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/xorg/xvkbd $homeDir/.xvkbd;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/xorg/Xresources $homeDir/.Xresources;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/xorg/Xsession $homeDir/.Xsession;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/vscode/settings.json $homeDir/.config/Code/User/settings.json;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln $scriptDir/vscode/keybindings.json $homeDir/.config/Code/User/keybindings.json;
-[[ $macos == false ]] && $ln $scriptDir/apt/sources.list /etc/apt/sources.list;
-[[ $macos == false ]] && $ln $scriptDir/apt/preferences /etc/apt/preferences;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln /usr/bin/firefox-dev /etc/alternatives/x-www-browser;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln /usr/bin/i3 /etc/alternatives/x-window-manager;
-[[ $cliOnly == false ]] && [[ $macos == false ]] && $ln /usr/bin/xterm /etc/alternatives/x-terminal-emulator;
-[[ $macos == false ]] && which proftpd > /dev/null && $ln $scriptDir/daemons/proftpd.conf /etc/proftpd/proftpd.conf;
-[[ $macos == false ]] && which smbd > /dev/null && $ln $scriptDir/daemons/smb.conf /etc/samba/smb.conf;
-[[ $macos == false ]] && which sshd > /dev/null && $ln $scriptDir/daemons/sshd.conf /etc/ssh/sshd_config;
+# Linux create directories
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $mkdir $homeDir/.config/i3;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $mkdir $homeDir/.config/rofi;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $mkdir $homeDir/.config/dunst;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $mkdir $homeDir/.config/gtk-3.0;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $mkdir $homeDir/.config/Code/User/;
+
+# Common create symlinks
+$ln $scriptDir/bash_profile $homeDir/.bash_profile;
+$ln $scriptDir/git_config $homeDir/.gitconfig;
+$ln $scriptDir/nano_config $homeDir/.nanorc;
+
+# MacOS create symlinks
+[[ "$cliOnly" == "false" ]] && [[ "$macos" == "true" ]] && $ln $scriptDir/vscode_settings.json $homeDir/Library/Application\ Support/Code/User/settings.json;
+[[ "$cliOnly" == "false" ]] && [[ "$macos" == "true" ]] && $ln $scriptDir/vscode_keybindings.json $homeDir/Library/Application\ Support/Code/User/keybindings.json;
+[[ "$cliOnly" == "false" ]] && [[ "$macos" == "true" ]] && $ln $scriptDir/spectacle_shortcuts.json $homeDir/Library/Application\ Support/Spectacle/Shortcuts.json;
+
+# Linux create symlinks
+[[ "$linux" == "true" ]] && $ln /usr/share/nano /usr/local/share/nano;
+[[ "$linux" == "true" ]] && $ln $scriptDir/keyboard/apple_keyboard_fi /etc/default/keyboard;
+[[ "$linux" == "true" ]] && $ln $scriptDir/apt_sources /etc/apt/sources.list;
+[[ "$linux" == "true" ]] && $ln $scriptDir/apt_preferences /etc/apt/preferences;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/i3_config $homeDir/.config/i3/config;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/conky_config $homeDir/.conkyrc;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/rofi_config $homeDir/.config/rofi/config;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/dunst_config $homeDir/.config/dunst/dunstrc;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/gtk_2 $homeDir/.gtkrc-2.0;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/gtk_3 $homeDir/.config/gtk-3.0/settings.ini;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/mimeapps $homeDir/.config/mimeapps.list;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/keyboard/apple_keyboard_fi.xkbmap $homeDir/.xkbmap;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/x_xvkbd $homeDir/.xvkbd;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/x_resources $homeDir/.Xresources;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/x_session $homeDir/.Xsession;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/vscode_settings.json $homeDir/.config/Code/User/settings.json;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln $scriptDir/vscode_keybindings.json $homeDir/.config/Code/User/keybindings.json;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln /usr/bin/firefox-dev /etc/alternatives/x-www-browser;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln /usr/bin/i3 /etc/alternatives/x-window-manager;
+[[ "$cliOnly" == "false" ]] && [[ "$linux" == "true" ]] && $ln /usr/bin/xterm /etc/alternatives/x-terminal-emulator;
+
+# Install additional things
+cat $scriptDir/vscode_extensions | xargs -L 1 code --install-extension;
 
 # Print info that execution finished
 echo "Installation finished."
