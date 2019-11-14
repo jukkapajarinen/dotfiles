5dc6a632bc648cf5da8581ee6704456495341d9c
Jukka Pajarinen <jukkapajarinen91@gmail.com>
2019-11-14 19:39:02 +0200

    Created rofi-shutdown shell script to handle dmenu based shutdowns reboots and logout via rofi.

---
+++ scripts/rofi-shutdown.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Specify dmenu options for rofi
+options="Shutdown\nReboot\nLock Screen\nLogout\nSuspend\nHibernate";
+
+# Show the rofi and listen to stdin
+rofi_action=$(echo -e "$options" | rofi -i -dmenu -selected-row 2);
+
+# Handle Shutdown action
+if [[ $rofi_action == "Shutdown" ]]; then
+  systemctl poweroff;
+
+# Handle Reboot action
+elif [[ $rofi_action == "Reboot" ]]; then
+  systemctl reboot;
+
+# Handle Lock Screen action
+elif [[ $rofi_action == "Lock Screen" ]]; then
+  i3lock -c 1e1d40;
+
+# Handle Logout action
+elif [[ $rofi_action == "Logout" ]]; then
+  i3-msg exit;
+
+# Handle Suspend action
+elif [[ $rofi_action == "Suspend" ]]; then
+  systemctl suspend;
+
+# Handle Hibernate action
+elif [[ $rofi_action == "Hibernate" ]]; then
+  systemctl hibernate;
+fi
+
+# Inform user that the script ended
+echo "Selected option: $rofi_action. Script completed.";
