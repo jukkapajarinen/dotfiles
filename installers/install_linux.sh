2aac31afc3fe66307b7a1b0f5e0da731293134d3
Jukka Pajarinen <jukkapajarinen91@gmail.com>
2020-04-13 16:51:01 +0300

    Added /etc/localtime symlink to install_linux script for setting timezone.

--- installers/install_linux.sh
+++ installers/install_linux.sh
@@ -32,20 +32,10 @@ $mkdir $homeDir/.config/gtk-3.0;
 $mkdir $homeDir/.config/kitty;
 $mkdir $homeDir/.config/Code/User/;
 
-# Linux create symlinks
+# Linux create dotfile symlinks
 $ln $scriptDir/.bash_profile $homeDir/.bash_profile;
 $ln $scriptDir/.gitconfig $homeDir/.gitconfig;
 $ln $scriptDir/.nanorc $homeDir/.nanorc;
-sudo $ln /usr/share/nano /usr/local/share/nano;
-sudo $ln $scriptDir/keyboard/keyboard_apple_fi /etc/default/keyboard;
-sudo $ln $scriptDir/packages/.apt_debian.list /etc/apt/sources.list;
-sudo $ln $scriptDir/packages/.apt_docker.list /etc/apt/sources.list.d/docker.list;
-sudo $ln $scriptDir/packages/.apt_megasync.list /etc/apt/sources.list.d/megasync.list;
-sudo $ln $scriptDir/packages/.apt_regolith.list /etc/apt/sources.list.d/regolith.list;
-sudo $ln $scriptDir/packages/.apt_spotify.list /etc/apt/sources.list.d/spotify.list;
-sudo $ln $scriptDir/packages/.apt_virtualbox.list /etc/apt/sources.list.d/virtualbox.list;
-sudo $ln $scriptDir/packages/.apt_vscode.list /etc/apt/sources.list.d/vscode.list;
-sudo $ln $scriptDir/packages/.apt_preferences /etc/apt/preferences;
 $ln $scriptDir/.i3 $homeDir/.config/i3/config;
 $ln $scriptDir/.conkyrc $homeDir/.conkyrc;
 $ln $scriptDir/.rofi $homeDir/.config/rofi/config;
@@ -61,9 +51,22 @@ $ln $scriptDir/.vscode_settings.json $homeDir/.config/Code/User/settings.json;
 $ln $scriptDir/.vscode_keybindings.json $homeDir/.config/Code/User/keybindings.json;
 $ln $scriptDir/.kitty.conf $homeDir/.config/kitty/kitty.conf;
 $ln $scriptDir/.alacritty.yml $homeDir/.alacritty.yml;
+
+# Linux create other symlinks
+sudo $ln /usr/share/zoneinfo/Europe/Helsinki /etc/localtime;
 sudo $ln /usr/bin/firefox-dev /etc/alternatives/x-www-browser;
 sudo $ln /usr/bin/i3 /etc/alternatives/x-window-manager;
 sudo $ln /usr/bin/kitty /etc/alternatives/x-terminal-emulator;
+sudo $ln /usr/share/nano /usr/local/share/nano;
+sudo $ln $scriptDir/keyboard/keyboard_apple_fi /etc/default/keyboard;
+sudo $ln $scriptDir/packages/.apt_debian.list /etc/apt/sources.list;
+sudo $ln $scriptDir/packages/.apt_docker.list /etc/apt/sources.list.d/docker.list;
+sudo $ln $scriptDir/packages/.apt_megasync.list /etc/apt/sources.list.d/megasync.list;
+sudo $ln $scriptDir/packages/.apt_regolith.list /etc/apt/sources.list.d/regolith.list;
+sudo $ln $scriptDir/packages/.apt_spotify.list /etc/apt/sources.list.d/spotify.list;
+sudo $ln $scriptDir/packages/.apt_virtualbox.list /etc/apt/sources.list.d/virtualbox.list;
+sudo $ln $scriptDir/packages/.apt_vscode.list /etc/apt/sources.list.d/vscode.list;
+sudo $ln $scriptDir/packages/.apt_preferences /etc/apt/preferences;
 
 # Print info that execution finished
 echo "Installation finished."
