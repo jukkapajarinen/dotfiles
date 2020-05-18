1c906805154b8d04e63b6cc4473b67ba33ef7e08
Jukka Pajarinen <jukkapajarinen91@gmail.com>
2020-05-18 22:00:03 +0300

    Added trusty polybar back to dotfiles as its now officially available in debian buster-backports.

---
+++ scripts/polybar-all-monitors.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ##############################################################################
+# Polybar multi monitors script
+# ##############################################################################
+
+# Terminate already running bar instances
+killall -q polybar;
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Set the primary monitor and add it first
+primary="HDMI-2";
+export MONITOR=$primary && polybar --reload topbar &
+
+# Launch polybar on all monitors
+for monitor in $(polybar -m | cut -d":" -f1); do
+  [ $monitor != $primary ] && export MONITOR=$monitor && polybar --reload topbar &
+done
+
+# Inform user that the script ended
+echo "Polybar launching finished.";
