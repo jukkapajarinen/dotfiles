abccd71bf33eb9ad1d39e2dc3548e86636eacbad
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2021-07-21 21:32:18 +0300

    Various new gnome settings.

--- scripts/gnome-setup.sh
+++ scripts/gnome-setup.sh
@@ -7,6 +7,11 @@
 gsettings set org.gnome.gnome-flashback desktop false; #default: true
 gsettings set org.gnome.gnome-flashback root-background true; #default: false
 gsettings set org.gnome.desktop.wm.preferences button-layout "close:appmenu"; #default: appmenu:close
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' #default: Adwaita
+gsettings set org.gnome.mutter dynamic-workspaces false #default: true
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 10 #default: 4
+gsettings set org.gnome.desktop.interface clock-show-seconds true #default: false
+gsettings set org.gnome.desktop.interface clock-show-weekday true #default: false
 
 # ##############################################################################
 # Unset default gnome keybindings
